0cf946e50fb627488576b8228231f5412d31ea2c
Nicholas Sliter <78503029+Nicholas-Sliter@users.noreply.github.com>
2022-08-24 18:51:47 -0400

    Chrome rewrite

--- Middlebury Go Fix_Chrome/background.js
+++ Middlebury Go Fix_Chrome/background.js
@@ -1,32 +1,10 @@
-
-/*
- *	File: background.js
- *	Abstract: Global JavaScript file to capture URL and control redirect.
- *	Version: 1.1.0
- *
- *	Copyright (C) 2012 Nate Beatty. All Rights Reserved.
- */
-
-// Redirect support listener
-// Call chrome.extension.sendRequest({redirect: < desiredURL (string) >});
-chrome.extension.onRequest.addListener(function(request, sender) {
-	console.log("Redirecting to " + request.redirect);
-    chrome.tabs.update(sender.tab.id, {url: request.redirect});
-});
-
-// Listen to all requests before they are sent
-// Redirect to go service if necessary
-var goServerUrl = "http://go.middlebury.edu/";
-var newUrl;
-chrome.webRequest.onBeforeRequest.addListener(
-	function(details) {
-		if (details.url.indexOf("://go/") != -1) {
-			var URLparts = details.url.split("go/");
-			newUrl = goServerUrl + URLparts[1];
+const MIDD_URL = "https://go.middlebury.edu/";
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) { // listener for tab opens
+	if (changeInfo.status == 'loading') {
+		if (tab.url.indexOf("http://go/") !== -1 || tab.url.indexOf("https://go/") !== -1) { // if the tab is a go URL
+			const newUrl = MIDD_URL + tab.url.split("go/")[1];
 			console.log("Redirecting to go URL: " + newUrl);
-			return {redirectUrl: newUrl};
+			chrome.tabs.update(tabId, { url: newUrl });
 		}
-  	},
-	{urls: ["<all_urls>"]},
-	["blocking"]
- );
+	}
+})
